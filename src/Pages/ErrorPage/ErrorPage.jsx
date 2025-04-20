3844d5e9e5e4bc115c81be07132c76d1fc7e60d5
Al Ifran <ifran15-2915@diu.edu.bd>
2025-04-20 10:11:41 +0600

    setup completed

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-white">
+      <img
+        className="w-1/2 max-w-md"
+        src="/images/shutterstock_774749455.webp"
+        alt="Error img"
+      />
+    </div>
+
+  );
+};
+
+export default ErrorPage;
