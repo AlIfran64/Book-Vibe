a7bb4dfebd5ae6dcd1bb28e4da4578e590d19da9
Al Ifran <ifran15-2915@diu.edu.bd>
2025-04-20 19:06:35 +0600

    updated

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FaRegStar } from "react-icons/fa";
 
 const Book = ({ book }) => {
 
-  const { image, bookName } = book;
+  const { image, bookName, author, category, rating } = book;
 
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
@@ -10,22 +11,29 @@ const Book = ({ book }) => {
         <img
           src={image}
           alt="Shoes"
-          className="rounded-xl w-28 h-40" />
+          className="rounded-xl w-52 h-72" />
       </figure>
       <div className="card-body">
         <div className='flex justify-start items-center gap-5'>
           <div className='bg-[rgba(35,190,10,0.05)] text-[rgba(35,190,10,1)] font-semibold px-4 py-1 rounded-2xl'>
-            <p>Young Adult</p>
+            <p className='text-lg'>Young Adult</p>
           </div>
           <div className='bg-[rgba(35,190,10,0.05)] text-[rgba(35,190,10,1)] font-semibold px-4 py-1 rounded-2xl'>
-            <p>Identity</p>
+            <p className='text-lg'>Identity</p>
           </div>
         </div>
-        <h2 className="card-title">{bookName}</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-        <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+        <h2 className="card-title text-2xl font-semibold">{bookName}</h2>
+        <p className='text-lg'>By: {author}</p>
+        <div className='flex justify-between items-center'>
+          <div>
+            <p className='text-lg'>{category}</p>
+          </div>
+          <div className='flex items-center justify-center gap-3'>
+            <p className='text-lg'>{rating}</p>
+            <FaRegStar className='text-lg' />
+          </div>
         </div>
+
       </div>
     </div>
   );
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -8,7 +8,7 @@ const Books = () => {
 
   return (
     <div className='w-11/12 mx-auto'>
-      <h1 className='text-4xl font-semibold text-center my-20'>Books</h1>
+      <h1 className='text-4xl font-semibold text-center mt-20 mb-8'>Books</h1>
 
 
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-10  place-items-center'>
